a9224cbcf69742e17703013feda6e609237ef6a2
tang_jingfan <tangjingfan@gmail.com>
2024-11-22 10:26:00 +0800

    feat: finish reset

--- p4/deck.cpp
+++ p4/deck.cpp
@@ -13,3 +13,15 @@ Deck::Deck() : next(0) {
     }
   }
 }
+
+void Deck::reset() {
+  next = 0;
+  int index = 0;
+  for (int suit = 0; suit < DIAMONDS; suit++) {
+    for (int spot = 0; spot < ACE; spot++) {
+      deck[index].suit = static_cast<Suit>(suit);
+      deck[index].spot = static_cast<Spot>(spot);
+      index++;
+    }
+  }
+}
