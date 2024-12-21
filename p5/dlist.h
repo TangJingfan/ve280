b58a0580d27ff06c79eb5bdb2c410990c624688a
tang_jingfan <tangjingfan@gmail.com>
2024-12-21 13:49:17 +0800

    feat: init p5

--- p5/dlist.h
+++ p5/dlist.h
@@ -1,68 +1,63 @@
 #ifndef __DLIST_H__
 #define __DLIST_H__
 
-class emptyList
-{
-    // OVERVIEW: an exception class
+class emptyList {
+  // OVERVIEW: an exception class
 };
 
-template <class T>
-class Dlist
-{
-    // OVERVIEW: contains a double-ended list of Objects
+template <class T> class Dlist {
+  // OVERVIEW: contains a double-ended list of Objects
 
- public:
+public:
+  // Operational methods
 
-    // Operational methods
+  bool isEmpty() const;
+  // EFFECTS: returns true if list is empy, false otherwise
 
-    bool isEmpty() const;
-    // EFFECTS: returns true if list is empy, false otherwise
+  void insertFront(T *op);
+  // MODIFIES this
+  // EFFECTS inserts o at the front of the list
 
-    void insertFront(T *op);
-    // MODIFIES this
-    // EFFECTS inserts o at the front of the list
-    
-    void insertBack(T *op);
-    // MODIFIES this
-    // EFFECTS inserts o at the back of the list
+  void insertBack(T *op);
+  // MODIFIES this
+  // EFFECTS inserts o at the back of the list
 
-    T *removeFront();
-    // MODIFIES this
-    // EFFECTS removes and returns first object from non-empty list
-    //         throws an instance of emptyList if empty
+  T *removeFront();
+  // MODIFIES this
+  // EFFECTS removes and returns first object from non-empty list
+  //         throws an instance of emptyList if empty
 
-    T *removeBack();
-    // MODIFIES this
-    // EFFECTS removes and returns last object from non-empty list
-    //         throws an instance of emptyList if empty
+  T *removeBack();
+  // MODIFIES this
+  // EFFECTS removes and returns last object from non-empty list
+  //         throws an instance of emptyList if empty
 
-    // Maintenance methods
-    Dlist();                                   // constructor
-    Dlist(const Dlist &l);                     // copy constructor
-    Dlist &operator=(const Dlist &l);          // assignment operator
-    ~Dlist();                                  // destructor
+  // Maintenance methods
+  Dlist();                          // constructor
+  Dlist(const Dlist &l);            // copy constructor
+  Dlist &operator=(const Dlist &l); // assignment operator
+  ~Dlist();                         // destructor
 
- private:
-    // A private type
-    struct node
-    {
-        node   *next;
-        node   *prev;
-        T      *op;
-    };
+private:
+  // A private type
+  struct node {
+    node *next;
+    node *prev;
+    T *op;
+  };
 
-    node   *first; // The pointer to the first node (NULL if none)
-    node   *last;  // The pointer to the last node (NULL if none)
+  node *first; // The pointer to the first node (NULL if none)
+  node *last;  // The pointer to the last node (NULL if none)
 
-    // Utility methods
+  // Utility methods
 
-    void removeAll();
-    // EFFECT: called by destructor/operator= to remove and destroy
-    //         all list elements
+  void removeAll();
+  // EFFECT: called by destructor/operator= to remove and destroy
+  //         all list elements
 
-    void copyAll(const Dlist &l);
-    // EFFECT: called by copy constructor/operator= to copy elements
-    //         from a source instance l to this instance
+  void copyAll(const Dlist &l);
+  // EFFECT: called by copy constructor/operator= to copy elements
+  //         from a source instance l to this instance
 };
 
 /*
